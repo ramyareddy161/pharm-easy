94adecb9c4dcee7fdd7a361620cb0aa162097beb
ramya.reddy161 <ramya.reddy161@gmail.com>
2019-03-30 14:17:14 +0530

    frontend

---
+++ medical-frontend/build/precache-manifest.8cdb7125f7b374edc661101728657229.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "1569e05183bc928b45a3cc5a11bbe021",
+    "url": "/static/bundles/static/media/my_cart_details.1569e051.png"
+  },
+  {
+    "revision": "78b00a4a9c8b2465170c47d0f55054fe",
+    "url": "/static/bundles/static/media/medicine_details.78b00a4a.png"
+  },
+  {
+    "revision": "ce95b263f8921219c06ce7dda751c714",
+    "url": "/static/bundles/static/media/medical_side_pic.ce95b263.png"
+  },
+  {
+    "revision": "7258a19e428b8f3d03aa46fbf72dd0c8",
+    "url": "/static/bundles/static/media/medical_cover_pic.7258a19e.png"
+  },
+  {
+    "revision": "ad8c972d15f4ae1b3073",
+    "url": "/static/bundles/static/js/runtime~main.c04ad13f.js"
+  },
+  {
+    "revision": "a0f1334d043dfa48fca9",
+    "url": "/static/bundles/static/js/main.6ff75454.chunk.js"
+  },
+  {
+    "revision": "e89a2c799eb7e60b51a4",
+    "url": "/static/bundles/static/js/2.58785d79.chunk.js"
+  },
+  {
+    "revision": "a0f1334d043dfa48fca9",
+    "url": "/static/bundles/static/css/main.65b47bc5.chunk.css"
+  },
+  {
+    "revision": "80767a328f41dfc347de14aa08612bfa",
+    "url": "/static/bundles/index.html"
+  }
+];
